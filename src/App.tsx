40e0d09966a568e9ec4672c0ba67d20357734693
Jake Sinclair <sinclairs1234@gmail.com>
2023-05-10 14:16:40 +0100

    modified:   src/App.tsx modified:   src/components/ListGroup.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import ListGroup from "./components/listGroup";
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import NewButton from "./components/NewButton";
+import { useState } from "react";
 
 function App() {
-  return <div><ListGroup /></div>;
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  };
+
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading={"Cities"}
+        onSelectItem={handleSelectedItem}
+      />
+
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+
+      <NewButton onClick={() => setAlertVisibility(true)}>My Button</NewButton>
+    </div>
+  );
 }
 
 export default App;
